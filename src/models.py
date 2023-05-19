4020ee354d04f85a0dbf9523c5083f1da8abe681
ditto <wojtek.konieczkowicz@gmail.com>
2023-05-19 16:07:30 +0200

    Slightly custom version of Swin backbone

---
+++ src/models.py
@@ -0,0 +1,33 @@
+import torch
+
+from monai.networks.nets.swin_unetr import SwinTransformer
+from monai.utils import ensure_tuple_rep
+
+
+class Backbone(torch.nn.Module):
+    def __init__(self, args):
+        super(Backbone, self).__init__()
+        self.model = SwinTransformer(
+            in_chans=1,
+            embed_dim=args.embedding_size,
+            window_size=ensure_tuple_rep(7, args.spatial_dims),
+            patch_size=ensure_tuple_rep(2, args.spatial_dims),
+            depths=[2, 2, 2, 2],
+            num_heads=[3, 3, 3, 3],  # TODO: [3, 6, 12, 24] originally
+            mlp_ratio=4.0,
+            qkv_bias=True,
+            drop_rate=0.0,
+            attn_drop_rate=0.0,
+            drop_path_rate=args.dropout_path_rate,
+            norm_layer=torch.nn.LayerNorm,
+            use_checkpoint=args.use_gradient_checkpointing,
+            spatial_dims=args.spatial_dims,
+        )
+
+    def forward(self, x):
+        x = self.model(x)[-1]  # Take the deepest feature map
+        b, dim, _, _, _ = x.shape
+
+        # Mean over spatial dimensions
+        # TODO: why did the authors use just the first subcube?
+        return torch.mean(x.view(b, dim, -1), dim=2)
