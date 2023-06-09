22530263b07a6fac209dfd20a605c8f06e51d0fe
ditto <wojtek.konieczkowicz@gmail.com>
2023-06-09 15:58:59 +0200

    Generate custom train/val split for FLARE2022

--- src/loaders.py
+++ src/loaders.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 
@@ -8,12 +9,24 @@ def get_ssl_data(data_dir: str):
 
 
 def get_finetune_data(data_dir: str):
-    img_paths = sorted((Path(data_dir)/Path('imgs')).glob('*.nii.gz'))
-    label_paths = sorted((Path(data_dir)/Path('labels')).glob('*.nii.gz'))
+    data_dir = Path(data_dir)
+    with open(data_dir/'split.json') as json_file:
+        split = json.load(json_file)
 
-    assert len(img_paths) == len(label_paths)
+    train_data = [
+        {
+            'img': data_dir/Path(f'imgs/{ct_id}_0000.nii.gz'),
+            'label': data_dir/Path(f'labels/{ct_id}.nii.gz')    
+        } 
+        for ct_id in split['train']
+    ]
 
-    data = [{'img': img_path, 'label': label_path} 
-        for img_path, label_path in zip(img_paths, label_paths)]
+    val_data = [
+        {
+            'img': data_dir/Path(f'imgs/{ct_id}_0000.nii.gz'),
+            'label': data_dir/Path(f'labels/{ct_id}.nii.gz')    
+        } 
+        for ct_id in split['val']
+    ]
 
-    return data[:30], data[30:31]  # TODO: use true split
+    return train_data, val_data
